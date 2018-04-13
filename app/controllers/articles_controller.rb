a9dfc70edc204a2abda341ab5f2a40fcbe27fac6
coenvb <coenvb@hotmail.com>
2018-04-12 20:31:12 -0700

    add edit, update, others

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
 
+def index
+    @articles = Article.all
+end
+
 def new
     @article = Article.new
+end
+
+def edit
+    @article = Article.find(params[:id])
 end
 
 def create
@@ -12,6 +20,16 @@ def create
         redirect_to article_path(@article)
     else
         render 'new'
+    end
+end
+
+def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:notice] = "New Article was edited"
+        redirect_to article_path(@article)
+    else
+        render 'edit'
     end
 end
 
