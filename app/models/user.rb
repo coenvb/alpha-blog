5bf934c5708710538293c6de8bf6cadfe2ecbe8a
coenvb <coenvb@hotmail.com>
2018-04-18 22:08:42 -0700

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
 
-end
+  validates :username, 
+    presence: true, 
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 3, maximum: 12 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 3, maximum: 12},
+    format: { with: VALID_EMAIL_REGEX }
+  end
